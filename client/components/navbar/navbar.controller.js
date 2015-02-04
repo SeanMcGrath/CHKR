d97bffe224239521fc118fe4c3cc0baf16c9d592
SeanMcGrath <srmcgrat@umass.edu>
2015-02-04 10:00:48 -0500

    fixed padding of page wrapper when navbar collapsed

--- client/components/navbar/navbar.controller.js
+++ client/components/navbar/navbar.controller.js
@@ -1,5 +1,22 @@
 'use strict';
 
+$.fn.animateRotate = function(angle, duration, easing, complete) {
+  return this.each(function() {
+    var $elem = $(this);
+
+    $({deg: 0}).animate({deg: angle}, {
+      duration: duration,
+      easing: easing,
+      step: function(now) {
+        $elem.css({
+           transform: 'rotate(' + now + 'deg)'
+         });
+      },
+      complete: complete || $.noop
+    });
+  });
+};
+
 angular.module('chkrApp')
   .controller('NavbarCtrl', function ($scope, $location, Auth) {
     $scope.menu = [{
@@ -23,5 +40,11 @@ angular.module('chkrApp')
 
     $scope.toggleMenu = function() {
       $('#wrapper').toggleClass('toggled');
+    };
+
+    $scope.flipArrow = function() {
+      $('#arrow').animateRotate(180, function() {
+        $('#arrow').animateRotate(-180);
+      });
     };
   });
