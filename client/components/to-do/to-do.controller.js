23c616dd4f20ef693c58de27d7dd7d2634ec7aa6
SeanMcGrath <srmcgrat@umass.edu>
2015-02-08 20:38:13 -0500

    date picking works, need to minimize input firm and save selection

--- client/components/to-do/to-do.controller.js
+++ client/components/to-do/to-do.controller.js
@@ -57,13 +57,35 @@ angular.module('chkrApp')
             });
     };
 
-    $scope.vm = {
-        message: "Bootstrap DateTimePicker Directive",
-        dateTime: {}
+    $scope.today = function() {
+        $scope.dt = new Date();
+    };
+    $scope.today();
+
+    $scope.clear = function () {
+        $scope.dt = null;
     };
 
-    $scope.$on('emit:dateTimePicker', function (e, value) {
-       $scope.vm.dateTime = value.dateTime;
-       console.log(value);
-    });
+    // Disable weekend selection
+    $scope.disabled = function(date, mode) {
+    };
+
+    $scope.toggleMin = function() {
+        $scope.minDate = $scope.minDate ? null : new Date();
+    };
+    $scope.toggleMin();
+
+    $scope.open = function($event) {
+        $event.preventDefault();
+        $event.stopPropagation();
+        $scope.opened = true;
+    };
+
+    $scope.dateOptions = {
+        formatYear: 'yy',
+        startingDay: 1
+    };
+
+    $scope.formats = ['dd-MMMM-yyyy', 'yyyy/MM/dd', 'dd.MM.yyyy', 'shortDate'];
+    $scope.format = $scope.formats[0];
   });
