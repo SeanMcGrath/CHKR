707146c7117ebbcfce3cc2a85a32693215b24274
SeanMcGrath <srmcgrat@umass.edu>
2015-02-16 15:02:15 -0500

    removed debugging messages, narrowed password change entry

--- client/app/main/main.controller.js
+++ client/app/main/main.controller.js
@@ -3,9 +3,7 @@
 angular.module('chkrApp')
   .controller('MainCtrl', function ($scope, Auth, $location, $window) {
 
-  	$scope.isLoggedIn = Auth.isLoggedIn;
-  	console.log($scope.isLoggedIn());
-
+        //ensure user logged in
   	if(!$scope.isLoggedIn()){
   		$location.path('/login');
   	}
